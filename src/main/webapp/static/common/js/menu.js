224e8029854dd65b479a49a4eaee5de81544c21a
Bert Zhu <269475408@qq.com>
2018-11-30 18:20:48 +0800

    Menu Modify

--- src/main/webapp/static/common/js/menu.js
+++ src/main/webapp/static/common/js/menu.js
@@ -6,34 +6,33 @@ $(function () {
 
     $("#side-menu").find("a").click(function () {
         var index = $("#side-menu").find("a").index(this);
-        var main_menu = $(this).parent().attr("class");
+        var childrenSpan = $(this).children("span").attr("class");
+        var main_menu = $(this).parent("li").attr("class");
         if (main_menu != "") {
             $.cookie("left-menu-current", index, {path: '/'});
             $("#side-menu").find("a").removeClass("active");
             $(this).addClass('active');
         }
-    });
 
-    $("#side-menu").find("li").click(function () {
-        var li_active = $(this).attr("class");
-        var index = $("#side-menu").find("li").index(this);
-        $("#side-menu").find("li").removeClass("mm-active");
-        $("#side-menu li").find("ul").removeClass("mm-show");
-        $.cookie("left-menu-left-navigation-block", index, {path: '/'});
-        if (li_active != undefined) {
-            $(this).addClass('mm-active');
-            $(this).children("ul").addClass('mm-show');
-        }
-        if (li_active == "") {
-            $("#side-menu").find("li").removeClass("mm-active");
-            $("#side-menu li").find("ul").removeClass("mm-show");
+        if (childrenSpan == undefined) {
+            if ($(this).parent("li").parent("ul").attr("class").indexOf("mm-show") < 0) {
+                $.cookie("left-menu-left-navigation-block", index, {path: '/'});
+                $("#side-menu").find("li").removeClass("mm-active");
+                $("#side-menu li").find("ul").removeClass("mm-show");
+            }
+        } else {
+            $.cookie("left-menu-left-navigation-block", index, {path: '/'});
         }
     });
 
     if ($.cookie("left-menu-left-navigation-block") != null) {
         var index = $.cookie("left-menu-left-navigation-block");
-        $("#side-menu").find("li").eq(index).addClass("mm-active");
-        $("#side-menu").find("li").eq(index).children("ul").addClass("mm-show");
+        var currentLi = $("#side-menu").find("li").eq(index);
+        var parents = currentLi.parents("li");
+        parents.addClass("mm-active");
+        parents.children("ul").addClass("mm-show");
+        currentLi.addClass("mm-active");
+        currentLi.children("ul").addClass("mm-show");
     } else {
         $("#side-menu").find("li").removeClass("mm-active");
         $("#side-menu li").find("ul").removeClass("mm-show");
@@ -46,15 +45,15 @@ $(function () {
         $("#side-menu").find("a").removeClass("active");
         $("#side-menu").find("a").eq(0).addClass("active");
     }
-    
-    if ($.cookie("right-navigate-title") != null) {
-    	var right_nav_title = $.cookie("right-navigate-title");
-    	if (right_nav_title == 0 || right_nav_title == "") {
-    		$("#right_navigate_title").text("首页");
-    	} else {
-    		$("#right_navigate_title").text($.cookie("right-navigate-title"));
-    	}
-    }
+    //
+    // if ($.cookie("right-navigate-title") != null) {
+    // 	var right_nav_title = $.cookie("right-navigate-title");
+    // 	if (right_nav_title == 0 || right_nav_title == "") {
+    // 		$("#right_navigate_title").text("首页");
+    // 	} else {
+    // 		$("#right_navigate_title").text($.cookie("right-navigate-title"));
+    // 	}
+    // }
 });
 
 function clickMenu(obj, url) {
